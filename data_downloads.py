c8464d7dec9d189b8944465f24f90506a15e3560
shuyu-M <shuyumeng6@gmail.com>
2024-12-18 00:16:15 -0500

    download the data from googledrive

--- data_downloads.py
+++ data_downloads.py
@@ -1,12 +1,35 @@
 import gdown
 
-# Public link to your data file (Google Drive link example)
-url = 'https://drive.google.com/drive/folders/1k0QI4B8QA_0HTdN41Y2CbdfX2GYLIKrN'  # Replace FILE_ID with the actual ID
-output = 'data.csv'
-
+# File ID and download URL
+file_id = "1er2Xp9qU0JQyEydZ1PXRkiLEw7vSK1O0"
+url = f"https://drive.google.com/uc?id=1er2Xp9qU0JQyEydZ1PXRkiLEw7vSK1O0"
+output = "fra_cleaned.csv"  # Name of the file to save locally
 
 # Download the file
-print("Downloading data...")
+print("Downloading the file...")
 gdown.download(url, output, quiet=False)
+print(f"File downloaded and saved as {output}")
 
-print(f"Data downloaded and saved as {output}")
+
+
+# File ID and download URL
+file_id = "1R1rPcjVKfReAAUpBceHb1uw733KdFzbZ"
+url = f"https://drive.google.com/uc?id={file_id}"
+output = "extracted_reviews_with_perfume_names.csv"  # Name the file appropriately
+
+# Download the file
+print("Downloading the file...")
+gdown.download(url, output, quiet=False)
+print(f"File downloaded and saved as {output}")
+
+import gdown
+
+# File ID and download URL
+file_id = "1kv3DkGu_NhOfFeHCi-gEFdKcjcgudtnP"
+url = f"https://drive.google.com/uc?id={file_id}"
+output = "perfume_reviews.jsonl"  # Name the file appropriately
+
+# Download the file
+print("Downloading the JSON file...")
+gdown.download(url, output, quiet=False)
+print(f"File downloaded and saved as {output}")
