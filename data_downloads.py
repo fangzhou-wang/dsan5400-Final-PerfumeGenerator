83c59a79d4117ad1fef4231780c7794ce3d9db52
shuyu-M <shuyumeng6@gmail.com>
2024-12-17 23:49:51 -0500

    download 他和

---
+++ data_downloads.py
@@ -0,0 +1,11 @@
+import gdown
+
+# Public link to your data file (Google Drive link example)
+url = 'https://drive.google.com/drive/folders/1k0QI4B8QA_0HTdN41Y2CbdfX2GYLIKrN'  # Replace FILE_ID with the actual ID
+output = 'data.csv'
+
+# Download the file
+print("Downloading data...")
+gdown.download(url, output, quiet=False)
+
+print(f"Data downloaded and saved as {output}")
