2d2762095a6a7ecec3becb3d5e14faca26a881ac
shuyu-M <shuyumeng6@gmail.com>
2024-12-17 23:53:15 -0500

    download data from google drive

--- data_downloads.py
+++ data_downloads.py
@@ -4,6 +4,7 @@ import gdown
 url = 'https://drive.google.com/drive/folders/1k0QI4B8QA_0HTdN41Y2CbdfX2GYLIKrN'  # Replace FILE_ID with the actual ID
 output = 'data.csv'
 
+
 # Download the file
 print("Downloading data...")
 gdown.download(url, output, quiet=False)
